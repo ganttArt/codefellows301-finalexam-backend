0b171af15383b80dd5e4f5973cd1bb2fa63bb25e
Chris Gantt <gantt.art@gmail.com>
2021-04-02 09:38:44 -1000

    Attempts at bug fixes

--- src/data.js
+++ src/data.js
@@ -4,23 +4,33 @@ const DataModel = require('./item-model.js');
 
 const Data = { };
 
-Data.addAnItem = async(req,res,next) => {
+Data.addAnItem = async (req, res, next) => {
   try {
     const data = req.body;
+    // console.log('next', next);
+    // console.log('request object', req.body, req.data, req.query, req.params);
+    // none of the data coming in from the request at all. Don't know where to find it.
+    // console.log('add an item data', data);
     const item = new DataModel(data);
-    res.status(404).json(item);
-  } catch(e) { next(e.message); }
+    // console.log('add an item', item);
+    res.status(200).json(item);
+  } catch(e) { 
+    next(e); 
+    // res.status(500).send(e.message);
+  }
 }
 
-Data.getAllItems = async(req, res) => {
-  const items = await DataModel.find({});
+Data.getAllItems = async (req, res) => {
+  const items = await DataModel.find();
   res.status(200).json(items);
 }
 
 Data.getOneItem = async(req, res) => {
-  const id = req.param.id;
-  const items = await DataModel.find({_id:id});
-  res.status(200).json(items[0]);
+  const id = req.params.id;
+  console.log('getoneitem', id);
+  const items = await DataModel.find({ _id:id });
+  console.log(items);
+  res.status(200).json(items);
 }
 
 Data.deleteOneItem = async(req, res) => {
@@ -28,8 +38,10 @@ Data.deleteOneItem = async(req, res) => {
 }
 
 Data.updateOneItem = async(req, res) => {
-  const id = req.param.id;
+  const id = req.params.id;
   const data = req.body;
+  console.log('update', id, data);
+  // data is a completely empty object, and it should be going here.
   const item = await DataModel.findByIdAndUpdate(id, data, {new:true, useFindAndModify:false});
   res.status(200).json(item);
 }
--- src/server.js
+++ src/server.js
@@ -2,15 +2,18 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 const Data = require('./data.js');
 
 app.use(express.urlencoded({extended:true}));
 
 app.get('/items', Data.getAllItems);
-app.get('/items/:id', getOneItem);
+app.get('/items/:id', Data.getOneItem);
 app.delete('/items/:id', Data.deleteOneItem);
 app.post('/items', Data.addAnItem);
+app.put('/items/:id', Data.updateOneItem);
 
 app.use('*', (req,res) => {
   res.status(404).send('These are not the droids you are looking for.');
